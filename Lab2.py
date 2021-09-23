b2ae9c5bb858da5b134be5faedb08f564dd03034
Will <wpgrimmer@loyola.edu>
2021-09-23 09:41:22 -0400

    hi terry

---
+++ Lab2.py
@@ -0,0 +1,9 @@
+# 31536000
+
+births=float(input("input births per second"))
+deaths=float(input("input deaths per second"))
+migrations=float(input("input migrations per second"))
+years=float(input("please input how many years into the future you wish to calculate"))
+initialPop=float(input("please input the initial population"))
+prediction=(births+deaths)*years
+
