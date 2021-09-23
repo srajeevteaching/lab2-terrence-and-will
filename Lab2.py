48d99e792e22e2fd464c70cad6edf8417060568b
Will <wpgrimmer@loyola.edu>
2021-09-23 10:03:57 -0400

    Finished

--- Lab2.py
+++ Lab2.py
@@ -1,9 +1,16 @@
-# 31536000
-
+conversionFactor=31536000
 births=float(input("input births per second"))
 deaths=float(input("input deaths per second"))
 migrations=float(input("input migrations per second"))
 years=float(input("please input how many years into the future you wish to calculate"))
 initialPop=float(input("please input the initial population"))
-prediction=(births+deaths)*years
+
+totalBirths=births*conversionFactor*years
+totalDeaths=deaths*conversionFactor*years
+totalMigrations=migrations*conversionFactor*years
+
+prediction=initialPop+totalBirths-totalDeaths+totalMigrations
+# print(totalBirths)
+# print(totalDeaths)
+print(prediction)
 
